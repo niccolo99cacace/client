3545290c4cfbac136bc1f89a00f8bc03fcbb2f61
99niccolo99 <napoliever52@gmail.com>
2022-11-21 18:29:40 +0100

    Any message - what you have done since your last commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from "./components/Search";
+import React from 'react';
 
 function App() {
+
+  const a = "aaa";
+  /*
+  const [patients, setPatients] = useState("JO");
+   const username = "j.parker@meddoctor.org";
+  const password = "DocJParker";
+  const url = 'http://141.95.111.168:8080/media/v1/patients/MMRMRA89L29Z404B';  
+
+  const fetchPatients = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPatients(data.results[0].title);
+      });
+  };
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Search></Search>
   );
 }
 
---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+function Search() { 
+    return (
+    <React.Fragment>
+        <Button variant="contained" size="large" >
+          aaaa
+        </Button>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </React.Fragment>
+  );
+}
+
+export default Search();
